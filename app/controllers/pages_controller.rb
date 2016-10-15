f5c67e524223e90cf7c1759f124b30b30263136f
Briana Eng <brianaeng@gmail.com>
2016-10-14 21:06:07 -0700

    Cleaning up code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
   def home
-    # Sort by num_votes (which is lowest to highest) then by negative id (because first I want the newest on top) and then I run reverse so the result it highest upvotes and highest id (aka newest)
-
     # @top_movies = @movies.sort_by { |movie_hash| [movie_hash[:num_votes], (-movie_hash[:id])] }.reverse.take(10)
 
     @top_movies = Movie.order('num_votes DESC, id').limit(10)
